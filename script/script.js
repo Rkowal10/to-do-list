cbc9baf52b6c0d60bbbef251d6cd657df275f64b
Radoslaw Kowalczyk <rkowal64@gmail.com>
2022-11-17 17:11:02 +0100

    added function render

--- script/script.js
+++ script/script.js
@@ -1,6 +1,31 @@
 {
-    const init = () => {
+    const tasks = [
+        {
+            content: "zjeść śnaidanie",
+            done: true,
+        },
+        {
+            content: "umyć naczynia",
+            done: false,
+        },
+    ];
 
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+               ${task.content} 
+            </li>
+            `;
+        }
+
+        document.querySelector("js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
     };
 
     init();
