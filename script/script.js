535a6b72ab6944796c778a0f28c11d8eee8abe37
Radoslaw Kowalczyk <rkowal64@gmail.com>
2023-01-02 12:29:03 +0100

    added immutability

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 {
-    const tasks = [];
+    let tasks = [];
 
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
@@ -16,7 +16,7 @@
             toggleDoneButton.addEventListener("click", () => {
                 toggleTaskDone(index);
             });
-        }); 
+        });
     };
 
     const render = () => {
@@ -46,20 +46,31 @@
     };
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent, done: false },
+        ];
 
         render();
     };
 
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasksLenght = tasks.length;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1, tasksLenght),
+        ];
+
         render();
     };
 
     const toggleTaskDone = (taskIndex) => {
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasksLenght = tasks.length;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            { ...tasks[taskIndex], done: !tasks[taskIndex].done },
+            ...tasks.slice(taskIndex + 1, tasksLenght),
+        ];
         render();
     };
 
